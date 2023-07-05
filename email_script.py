c20c434542bb19111a55ca639a28151059c6e07e
Pratik Rana <136930578+pratikranaa@users.noreply.github.com>
2023-07-05 15:43:00 +0530

    Update email_script.py

--- email_script.py
+++ email_script.py
@@ -6,7 +6,7 @@ from email.mime.application import MIMEApplication
 def send_application(smtp_server, smtp_port, sender_email, password, recipient_email, subject, body, attachment_path):
     # Create a multipart message and set headers
     message = MIMEMultipart()
-    message["From"] = "pratikrana859@gmail.com"
+    message["From"] = "sender_email"
     message["To"] = recipient_email
     message["Subject"] = subject
 
@@ -39,8 +39,8 @@ smtp_server = "smtp.gmail.com"  # Replace with your SMTP server address
 smtp_port = 587  # Replace with your SMTP server port
 
 # Email content
-subject = "Looking for Backend Developer Position PHP/Python"
-body = "Sir/Mam,\nGood Afternoon\n\nI hope this email finds you well. I am writing to express my strong interest in pursuing a Backend Developer position at {company_name}. As a passionate and driven individual with a solid foundation in backend development, I am eager to apply and expand my skills in a professional setting.\n\nI am currently pursuing my Bachelor's degree in Computer Science and Artificial Intelligence, and I have gained practical experience through various personal projects and coursework. My proficiency lies in languages such as Python, PHP, and C++, and I have hands-on experience with frameworks like Django and Laravel. Additionally, I have worked with databases like MySQL and PostgreSQL, and I am adept at utilizing version control systems like Git.\n\nDuring my academic journey, I have developed a strong understanding of backend development principles, API design, and database management. I am eager to apply my theoretical knowledge in a practical setting, and I believe that an opportunity at {company_name} would provide me with invaluable exposure to real-world projects and industry best practices.\n\nI would be thrilled to have the opportunity to contribute to {company_name}'s success and learn from your experienced team of professionals.\n\nPlease find attached my resume, which provides further details about my skills, projects, and academic background.\nThank you for considering my application. I am available at your convenience for any further discussion. I look forward to the possibility of joining your team.\n\nThank you for your time and consideration.\n\nRegards,\nPratik Rana"
+subject = "Looking for Job"
+body = "Sir/Mam,\nGood Afternoon\n\nI hope this email finds you well. I would be thrilled to have the opportunity to contribute to {company_name}'s success. Thank you for considering my application. I am available at your convenience for any further discussion. \n\nRegards"
 
 # File containing email addresses
 email_list_file = "email_list.txt"
